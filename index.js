7b90eb0667dbbee8a642c58f90cc5faccc7e3d39
Thanh Huy <vothanhhuy7102004@gmail.com>
2025-12-29 15:19:25 +0700

    4

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 1. API Lấy danh sách xe (Vẫn giữ nguyên vì đang chạy tốt)
-app.all('/api/v/de1', (req, res) => {
+// 1. API Lấy danh sách xe (Sửa lại phần GIA_BAN)
+app.get('/api/v/de1', (req, res) => {
     const ds_xe = [
-        { "BIEN_SO": "29A-12345", "TEN_XE": "Toyota Vios", "GIA_BAN": "350.000.000" },
-        { "BIEN_SO": "30H-99999", "TEN_XE": "Mercedes C200", "GIA_BAN": "1.200.000.000" },
-        { "BIEN_SO": "51G-55555", "TEN_XE": "Honda City", "GIA_BAN": "450.000.000" }
+        { 
+            "BIEN_SO": "29A-12345", 
+            "TEN_XE": "Toyota Vios", 
+            "GIA_BAN": 350000000 // ĐỂ SỐ THUẦN TÚY, KHÔNG DẤU CHẤM, KHÔNG NGOẶC KÉP
+        },
+        { 
+            "BIEN_SO": "30H-99999", 
+            "TEN_XE": "Mercedes C200", 
+            "GIA_BAN": 1200000000 
+        },
+        { 
+            "BIEN_SO": "51G-55555", 
+            "TEN_XE": "Honda City", 
+            "GIA_BAN": 450000000 
+        }
     ];
     res.json({ "DS_XE_CU": ds_xe });
 });
